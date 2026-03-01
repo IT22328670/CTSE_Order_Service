93f3133bdd6241938ee295f9cb0598b2170161df
IT22328670 <it22328670@my.sliit.lk>
2026-03-02 01:55:14 +0530

    setup the backend

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Order Service is running!');
+});
+
+const PORT = process.env.PORT || 5003;
+
+app.listen(PORT, () => {
+  console.log(`Order Service is running on port ${PORT}`);
+});
